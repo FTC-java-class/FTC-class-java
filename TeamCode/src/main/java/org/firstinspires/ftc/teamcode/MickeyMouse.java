4544777f43a2ccdb31f959e59e73c8a4827d9c52
N3M0000 <kiranmusson@gmail.com>
2024-08-14 20:21:10 -0700

    teleop basic
    teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MickeyMouse.java
@@ -0,0 +1,107 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import java.awt.font.NumericShaper;
+
+@TeleOp(name = "MickeyMouse", group = "Teleop")
+public class MickeyMouse extends OpMode {
+    DcMotor frontleftdrive = null;
+    DcMotor frontrightdrive = null;
+    DcMotor backleftdrive = null;
+    DcMotor backrightdrive = null;
+    //Controller buttons
+    boolean aPress1;
+    boolean bPress1;
+
+    boolean aPress1Down = false;
+    boolean bPress1Down = false;
+
+    boolean dpadDown1;
+    boolean dpadUp1;
+    boolean dpadDown1_down = false;
+    boolean dpadup1_down = false;
+
+    //For joystick
+    double frontrightcurrent = 0;
+    double frontleftcurrent = 0;
+    double backrightcurrent = 0;
+    double backleftcurrent = 0;
+    double driverTurn = 0;
+    double gamepadXControl = 0;
+    double gamepadYControl = 0;
+    double gamepadXCoordinate = 0;
+    double gamepadYCoordinate = 0;
+    double gamepadDegree = 0;
+    //double robotDegree = 0;
+    double movementDegree = 0;
+    double gamepadHypot = 0;
+    double power = 0.2;
+    //
+    static long loopstart = System.currentTimeMillis();
+    @Override
+    public void init() {
+        frontleftdrive = hardwareMap.get(DcMotor.class, "front_left_drive");
+        frontrightdrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backleftdrive = hardwareMap.get(DcMotor.class, "back_left_drive");
+        backrightdrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+
+    }
+    public void loop() {
+        LOOPSTART();
+        updateMotor();
+        updateController();
+        telemetry.addData("power", power);
+        telemetry.addData("Frame Per Second", 1000/GETLOOPTIME());
+        telemetry.update();
+    }
+    public static void LOOPSTART() {
+        loopstart = System.currentTimeMillis();
+    }
+    public static double GETLOOPTIME() {
+        return((double)(System.currentTimeMillis() - loopstart));
+    }
+    public void updateController() {
+        dpadDown1_down = gamepad1.dpad_down && !dpadDown1;
+        dpadDown1 = gamepad1.dpad_down;
+        dpadup1_down = gamepad1.dpad_up && !dpadUp1;
+        dpadUp1 = gamepad1.dpad_up;
+
+        if (dpadup1_down) {
+            power = power+0.2;
+        }
+        if (dpadDown1_down) {
+            power = power-0.2;
+        }
+
+    }
+
+    public void updateMotor() {
+        gamepadXCoordinate = gamepad1.left_stick_x;
+        gamepadYCoordinate  = gamepad1.left_stick_y;
+
+        //Angle of the robot
+        gamepadHypot = Range.clip(Math.hypot(gamepadXCoordinate, gamepadYControl), 0, 1);
+        gamepadDegree = Math.atan2(gamepadYCoordinate, gamepadXCoordinate);
+
+        movementDegree = gamepadDegree;
+        gamepadYControl = Math.sin(movementDegree) * gamepadHypot;
+        gamepadXControl = Math.cos(movementDegree) * gamepadHypot;
+        frontleftdrive.setPower(frontleftcurrent);
+        frontrightdrive.setPower(frontrightcurrent);
+        backleftdrive.setPower(backleftcurrent);
+        backrightdrive.setPower(backrightcurrent);
+        frontrightcurrent = (power * (driverTurn+ (-gamepadYControl) + gamepadXControl));
+        backrightcurrent = (power * (driverTurn+ (-gamepadYControl) - gamepadXControl));
+        frontleftcurrent = (power * (driverTurn+ (-gamepadYControl) - gamepadXControl));
+        backleftcurrent = (power * (driverTurn+ (-gamepadYControl) + gamepadXControl));
+        driverTurn = gamepad1.right_stick_x;
+
+
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/something/niofgbnaiokb.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.something;
+
+public class niofgbnaiokb {
+}
